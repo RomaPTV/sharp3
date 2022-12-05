ecac33111f5ed9636b2ddf01c4d5d3ddc32841a8
Roman <poteev.roman@mail.ru>
2022-12-06 00:13:48 +0300

    final

--- Program.cs
+++ Program.cs
@@ -47,14 +47,89 @@ Console.WriteLine(year);
 /*string Method4(int count, string c)
 {
 
-    int i = 0;
+
     string result = string.Empty;
+    for (int i = 0; i < count; i++)
     {
-        while (i < count);
-        i++;
+        result + text;
     }
     return result;
 }
-string res = Method4(5, "ZOV"); 
+*/
+/*
+    for (int i = 2; i <= 10; i++)
+    {
+
+        for (int j = 0; j <= 10; j++)
+        {
+
+            Console.WriteLine($"{i} x {j} = {i * j}");
+
+        }
+
+    }
+Console.WriteLine();
+*/
+/*
+string text = "— Я думаю, — сказал князь, улыбаясь, — что,"
+                + "ежели бы вас послали вместо нашего милого "
+                + "Винценгероде, вы бы взяли приступом согласие"
+                + "прусского короля. Вы так красноречивы. Вы"
+                + "дадите мне чаю?";
+
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+        {
+            if(text[i] == oldValue) result = result + $"{newValue}";
+            else result = result + $"{text[i]}";
+        }        
+    return result;
+
+}
+Console.WriteLine();
+string newText = Replace(text,'к', 'К' );
+Console.WriteLine(newText);
 */
 
+int[] arr = {1, 5, 8 ,6 ,4 ,7 ,4 ,2 ,4, 9, 4, 6, 7};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine($"{array[i]}");
+        
+    }
+    Console.WriteLine();
+
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int minPosition = i;
+        
+        for (int j = i + 1; j < array.Length - 1; j++)
+        {
+            if(array[j] < array [minPosition]) minPosition = j; 
+
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+
+    }
+
+}
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
