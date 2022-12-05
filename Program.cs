263563876dd8205110f44b5580fbed38adb48582
Roman <poteev.roman@mail.ru>
2022-12-05 22:59:48 +0300

    ver1

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿//Вид 1
+/* void Method1()
+{
+
+Console.WriteLine("Автор ...");
+
+}
+Method1();
+
+//Вид 2
+
+/*void Method2(string msg)
+{
+    Console.WriteLine();
+}
+Method2(msg:"текст сообщения");
+
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+
+            Console.WriteLine(msg);
+            i++;
+
+    }
+
+
+}
+Method21(msg: "Текст ", count: 4);
+
+//Вид 3
+
+int Method3()
+{
+
+    return DateTime.Now.Year;
+
+}
+int year = Method3();
+Console.WriteLine(year);
+*/
+//Вид 4
+
+/*string Method4(int count, string c)
+{
+
+    int i = 0;
+    string result = string.Empty;
+    {
+        while (i < count);
+        i++;
+    }
+    return result;
+}
+string res = Method4(5, "ZOV"); 
+*/
+
